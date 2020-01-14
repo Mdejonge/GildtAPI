d583a67ce9304dc74d4ad04ed0ea63ecdd99644f
Tim Wolfram <timothy_wolfram@hotmail.com>
2020-01-14 20:19:03 +0100

    whitelister class

---
+++ Whitelister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GildtAPI
+{
+    static class Whitelister
+    {
+        static readonly Regex ALPHA = new Regex("[^ [:alpha:]]+");
+        /// <summary>
+        /// Gets string filtered through regex only allowing alphabet characters and spaces.
+        /// </summary>
+        public static string GetAlphaFiltered(string input)
+        {
+            return ALPHA.Replace(input, "");
+        }
+    }
+}
